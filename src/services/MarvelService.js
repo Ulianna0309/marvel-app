4126ebd6389ef7afa253a8ea7d5f769c5855bff2
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2023-01-03 12:14:59 +0200

    148

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,4 +1,7 @@
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=6a4ecc9c3e672e2af4f94b05a82f5ca5';
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -11,7 +14,12 @@ class MarvelService {
 
 
     getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=6a4ecc9c3e672e2af4f94b05a82f5ca5');
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
 
     }
 }
