13dfb74166437b648b4428ccb23cf714fb14dcb7
yulia_vishnevskaya <yulia_vishnevskaya@tchdm.live>
2023-01-03 20:23:21 +0200

    152 start

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -32,6 +32,12 @@ class  RandomChar extends Component{
         })
     }
 
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
     onError = () => {
         this.setState({
             loading: false,
@@ -41,6 +47,7 @@ class  RandomChar extends Component{
 
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.onCharLoading();
         this.marvelService
             .getCharacter(id)
             .then(this.onCharLoaded)
